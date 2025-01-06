9c36fa7c075a1d3f062296f27545899d1bacaf8a
YassineCH02 <chagdaniyassincv@gmail.com>
2025-01-06 05:11:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("artForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for sharing your art! We will review and feature it soon.");
+});
